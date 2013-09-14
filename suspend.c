90282a987dc9f57d1c97ea714ffdfce1b426498d
0mg <0millig@gmail.com>
2013-09-14 21:57:38 +0900

    dev: release DLL after used

--- suspend.c
+++ suspend.c
@@ -3,5 +3,6 @@ int WINAPI WinMain(HINSTANCE hi, HINSTANCE hp, LPSTR cl, int cs) {
   HMODULE lib = LoadLibrary(TEXT("powrprof.dll"));
   FARPROC SetSuspendState = GetProcAddress(lib, "SetSuspendState");
   SetSuspendState(FALSE, FALSE, FALSE);
+  FreeLibrary(lib);
   return 0;
 }
