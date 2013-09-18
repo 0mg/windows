09a4325f933ad3df9742eca171c78455e8b9130d
0mg <0millig@gmail.com>
2013-09-18 17:58:20 +0900

    upd: dispoff, suspend > more safety flow if nostartfiles

--- dispoff.c
+++ dispoff.c
@@ -1,4 +1,9 @@
+#define UNICODE
 #include <windows.h>
+void __start__() {
+  // program will start from here if `gcc -nostartfiles`
+  ExitProcess(WinMain(GetModuleHandle(NULL), 0, "", 0));
+}
 int WINAPI WinMain(HINSTANCE hi, HINSTANCE hp, LPSTR cl, int cs) {
   SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
   return 0;
--- suspend.c
+++ suspend.c
@@ -1,4 +1,9 @@
+#define UNICODE
 #include <windows.h>
+void __start__() {
+  // program will start from here if `gcc -nostartfiles`
+  ExitProcess(WinMain(GetModuleHandle(NULL), 0, "", 0));
+}
 int WINAPI WinMain(HINSTANCE hi, HINSTANCE hp, LPSTR cl, int cs) {
   HMODULE lib = LoadLibrary(TEXT("powrprof.dll"));
   FARPROC SetSuspendState = GetProcAddress(lib, "SetSuspendState");
