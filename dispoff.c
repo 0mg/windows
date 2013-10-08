277e9e6b5e44f744552acd3ff32041e7560e4f24
0mg <0millig@gmail.com>
2013-10-08 13:47:54 +0900

    fix: dispoff > exit after ran

--- dispoff.c
+++ dispoff.c
@@ -3,9 +3,9 @@
 #include <windows.h>
 void __start__() {
   // program will start from here if `gcc -nostartfiles`
-  ExitProcess(WinMain(GetModuleHandle(NULL), 0, "", 0));
+  ExitProcess(WinMain(0, 0, NULL, 0));
 }
 int WINAPI WinMain(HINSTANCE hi, HINSTANCE hp, LPSTR cl, int cs) {
-  SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+  SendNotifyMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
   return 0;
 }
