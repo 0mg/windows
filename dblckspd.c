b7c21eba0aa33815a446b111f2492e1e6bdaa429
0mg <millig@outlook.jp>
2014-08-23 21:12:29 +0900

    dblckspd upd: enable cmdline option
    dblckspd.exe 200 //set double click time to 200ms

--- dblckspd.c
+++ dblckspd.c
@@ -6,7 +6,8 @@ void __start__() {
   ExitProcess(WinMain(0, 0, NULL, 0));
 }
 int WINAPI WinMain(HINSTANCE hi, HINSTANCE hp, LPSTR cl, int cs) {
-  SystemParametersInfo(SPI_SETDOUBLECLICKTIME, 200, NULL,
+  SystemParametersInfo(SPI_SETDOUBLECLICKTIME,
+    _wtoi(CommandLineToArgvW(GetCommandLineW(), &cs)[1]) ?: 200, NULL,
     SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
   return 0;
 }
