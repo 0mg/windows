dcaa07f7b054c10f294e099d558f563682d5f9cf
0mg <millig@outlook.jp>
2014-11-13 23:37:06 +0900

    upd: makable dblckspd.c with VC++

--- dblckspd.c
+++ dblckspd.c
@@ -1,13 +1,15 @@
 #define UNICODE
 #pragma comment(lib, "user32.lib")
+#pragma comment(lib, "shell32.lib")
 #include <windows.h>
 void __start__() {
   // program will start from here if `gcc -nostartfiles`
   ExitProcess(WinMain(0, 0, NULL, 0));
 }
 int WINAPI WinMain(HINSTANCE hi, HINSTANCE hp, LPSTR cl, int cs) {
+  int n, ms = _wtoi(CommandLineToArgvW(GetCommandLineW(), &n)[1]);
   SystemParametersInfo(SPI_SETDOUBLECLICKTIME,
-    _wtoi(CommandLineToArgvW(GetCommandLineW(), &cs)[1]) ?: 200, NULL,
+    ms ? ms : 200, NULL,
     SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
   return 0;
 }
