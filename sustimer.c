88604ca564459452972d4a5c890958af3bb91bca
0mg <millig@outlook.jp>
2014-11-15 19:30:45 +0900

    dev: makable with /ENTRY:__start__ @ VC++

--- sustimer.c
+++ sustimer.c
@@ -19,6 +19,7 @@ DECLARE_INTERFACE(INTERFACE) {
   STDMETHOD(SetProgressValue)(THIS_ HWND, ULONGLONG, ULONGLONG);
 };
 #undef INTERFACE
+#pragma comment(lib, "kernel32.lib")
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "gdi32.lib")
 #pragma comment(lib, "shell32.lib")
